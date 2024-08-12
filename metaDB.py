624e81184ccb5f6bd59ad668f089d703096f45e5
apartsin <apartsin@gmail.com>
2024-08-12 12:22:56 +0300

    metadata done

---
+++ metaDB.py
@@ -0,0 +1,91 @@
+from baseAgent import BaseAgent
+from sqlStore import SQLStore
+import logging,uuid
+from pydantic import BaseModel
+
+user_info={"user_name":"John","user_role":"Fleet manager"}
+
+org_info={"org_name":"Egged",
+          "org_descr":"bus public transportation",
+          "org_conn_str":"dbname='acmebus' user='taskanalytics' password='leningrad' host='localhost' port='5432'",
+          "org_data_app":"telematics solution"}
+
+
+#generate job role responsibilities and tasks for each generated responsibility
+class RoleResponsibility(BaseModel):
+    job_responsibility: str 
+    responsibility_tasks: list[str]
+    
+class RoleResponsibilities(BaseModel):
+    job_responsibilities: list[RoleResponsibility]
+  
+class MetaDB:
+    def __init__(self):
+        self.store=SQLStore().connect(db_name="taskanalytics")
+        self.llm=BaseAgent()
+            
+    def create_or_update_user(self,user_info,org_info):
+        user_info["user_org_uid"]=self.create_or_update_org(org_info)
+        user_name=user_info["user_name"]
+        user_info["user_uid"]=self.store.find_obj("users","user_uid",f"user_name='{user_name}'")
+        return self.store.insert_or_update_obj("users","user_uid",user_info)
+        
+    def create_or_update_org(self, org_info):
+        org_name=org_info["org_name"]
+        org_info["org_uid"]=self.store.find_obj("orgs","org_uid",f"org_name='{org_name}'")
+        return self.store.insert_or_update_obj("orgs","org_uid",org_info)
+        
+    def delete_org(self,org_uid):
+        self.delete_org_users(org_uid)
+        self.store.del_obj("orgs","orgs_uid",org_uid)   
+        
+    def delete_user(self, user_uid):
+        self.delete_user_tasks(user_uid)
+        self.store.del_obj("users", "user_uid",user_uid)       
+        
+    def delete_task(self, task_uid): 
+        self.delete_task_kpis(task_uid)
+        self.store.del_obj("tasks", "task_uid",task_uid)   
+        
+    def delete_org_users(self,org_uid): 
+        org_users_ids=self.store.fetch_refs("users","user_uid","user_org_id",org_uid)
+        for user_uid in org_users_ids: self.delete_user(user_uid)
+        
+    def delete_user_tasks(self, user_uid):
+        user_tasks_ids=self.store.fetch_refs("tasks","task_uid","task_user_uid",user_uid)
+        for task_uid in user_tasks_ids: self.delete_task(task_uid)
+          
+    def delete_task_kpis(self, task_uid):
+        task_kpis_ids=self.store.fetch_refs("kpis","kpi_uid", "kpi_task_uid", task_uid)
+        self.store.del_objs("kpis", "kpi_uid",task_kpis_ids)
+        self.store.del_obj("task","task_uid",task_uid)
+        
+    def generate_user_tasks(self,user_uid):
+        user_info=self.store.fetch_obj("users","user_uid",user_uid)
+        org_info=self.store.fetch_obj("orgs","org_uid",user_info['user_org_uid'])
+        sys_msg="You are a helpful business analyst"
+        user_msg=f"Describe job responsibilities and tasks for each responsibility for job role:'{user_info['user_role']}' at {org_info['org_descr']}"
+        role_responsibilities=self.llm.struct_query(sys_msg, user_msg,RoleResponsibilities)
+        return self.insert_tasks(role_responsibilities.job_responsibilities,user_info['user_uid'])
+        
+    def insert_tasks(self, responsibilities, user_uid):
+        self.delete_user_tasks(user_uid)
+        for responsibility in responsibilities:
+            for task in responsibility.responsibility_tasks:
+                task_info={"task_name":task,"task_responsibility": responsibility.job_responsibility, "task_user_uid":user_uid}
+                self.store.insert_or_update_obj("tasks","task_uid",task_info)
+                
+    def fetch_schema(self, org_uid):
+        org_info=self.store.fetch_obj("orgs","org_uid",org_uid)
+        remote_ds=SQLStore().connect_str(org_info["org_conn_str"])
+        tables=remote_ds.fetch_tables()
+        for t in tables:self.insert_cols(t,remote_ds.fetch_columns(t["tablename"]),org_uid)
+    
+    def insert_cols(self,t,cols,org_uid):
+        col_dict=[{'col_uid':str(uuid.uuid4()),'col_name':c['column_name'],'col_type':c['data_type'],"table_name":t["tablename"],"table_org_uid":org_uid} for c in cols]
+        col_names=",".join(k for k in col_dict[0].keys())
+        vals=[','.join([f"'{v}'" for v in c.values()]) for c in col_dict]
+        mvals=",".join(f"({v})" for v in vals)
+        return self.store.execute(f"insert into cols ({col_names}) values {mvals}")
+            
+        
--- sqlStore.py
+++ sqlStore.py
@@ -3,7 +3,8 @@ postgres_cfg={
         "password": "leningrad",
         "def_db":"postgres"}
 
-import psycopg2,logging
+import psycopg2,logging, uuid
+from psycopg2.extras import RealDictCursor
 
 class SQLStore:
     
@@ -18,26 +19,59 @@ class SQLStore:
             self.conn=None
         return self
     
+    def connect_str(self,conn_str,autocommit=True):
+        self.conn=psycopg2.connect(conn_str) 
+        self.conn.autocommit=autocommit
+        return self
+    
     def connect(self,db_name=None,autocommit=True):
         self.close()
         if not db_name: db_name=self.cfg["def_db"]
         logging.info(f"connecting to {db_name}")
-        self.conn=psycopg2.connect(f"dbname={db_name} user={self.cfg['user']} password={self.cfg['password']}") 
-        self.conn.autocommit=autocommit
-        return self
-    
+        return self.connect_str(f"dbname={db_name} user={self.cfg['user']} password={self.cfg['password']}",autocommit) 
+        
     def execute(self, stmt):
         logging.info(f"executing {stmt}")
         self.conn.cursor().execute(stmt)
         return self
     
     def fetchall(self,stmt):
-        with self.conn.cursor() as c:
+        with self.conn.cursor(cursor_factory=RealDictCursor) as c:
             logging.info(f"fetching{stmt}")
             c.execute(stmt)
             res=c.fetchall()
         return res
     
+    def find_obj(self,table_name,pkey_name,criteria):
+        logging.info(f"find obj in {table_name} by {criteria}")
+        res=self.fetchall(f"select {pkey_name} from {table_name} where {criteria}")
+        return res[0][pkey_name] if len(res) else None
+    
+    def fetch_obj(self,table_name,pkey_name,uid):
+        logging.info(f"find obj in {table_name} by {uid}")
+        res=self.fetchall(f"select * from {table_name} where {pkey_name}='{uid}'")
+        return res[0] if len(res) else None
+    
+    def del_obj(self, table_name, pkey_name, uid): self.execute(f"delete * from {table_name} where {pkey_name}='{uid}'")
+    def del_objs(self, table_name, pkey_name, uids):
+        vals=",".join(f"'{uid}'" for uid in uids)
+        self.execute("delete * from {table_name} where {pkey_name} in ({vals})")
+    
+    def fetch_refs(self, table_name, pkey_name, fkey_name,value):
+        res=self.fetchall(f"select {pkey_name} from {table_name} where '{fkey_name}'='{value}'")
+        return [r[0] for r in res]
+    
+    def insert_or_update_obj(self, table_name, pkey_name, values):
+        if values.get(pkey_name,None):
+            vals=",".join([f"{k}='{v}'" for k,v in values.items() if k!=pkey_name])
+            self.execute(f"update {table_name} SET {vals} where {pkey_name}='{values[pkey_name]}'")
+        else:
+            values[pkey_name]=str(uuid.uuid4())
+            cols=",".join([k for k in values.keys()])
+            vals=",".join([f"'{v}'" for v in values.values()])
+            self.execute(f"insert into {table_name} ({cols}) VALUES ({vals})")
+        return values[pkey_name]
+            
     def create_db(self,db_name,overwrite=True):
         if overwrite: self.drop_db(db_name)
         return self.connect().execute(f"create database {db_name}").connect(db_name)
